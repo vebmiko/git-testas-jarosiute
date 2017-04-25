64101bd45f6468f5bcc34b90e2334353aeb39212
Kamile Jarosiute <kjarosiute@gmail.com>
2017-04-25 09:43:05 +0300

    Naujas Date funkcionalumas

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,8 +7,12 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
+
+SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = new Date();
+		String data = dateFormat.format(date);
 		System.out.println("HELLO UGLY WORLD!");
 		System.out.println("The World is not ugly!");
 
-	}
+ System.out.println("Siandienos data: " + data);	}
 }

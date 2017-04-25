ad39fd5cbc88d456e7a0c5c090362869dbbbc1a3
Kamile Jarosiute <kjarosiute@gmail.com>
2017-04-25 09:28:54 +0300

    Naujas funkcionalumas

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,6 +9,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!");
 		System.out.println("The World is not ugly!");
+		Input in = new Input();
+		System.out.println("Iveskite savo varda:");
+		System.out.println("Hello " + in.input() + " !");
 
 	}
 }
---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+	Scanner sc = new Scanner(System.in);
+
+	public String input() {
+		String s = sc.nextLine();
+
+		return s;
+	}
+
+}
